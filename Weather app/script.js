aa0593fe5c91b990ef743faf8f278d304a27524e
Bello Joseph <joexef87@gmail.com>
2023-08-02 11:14:45 +0100

    Update script.js

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,54 +1,111 @@
-let currentDate = new Date();
-let presentDay = document.getElementById("current-day");
-let MonthAndYear = document.getElementById("Month-and-Year");
+const presentDay = document.getElementById("current-day");
+const monthAndYear = document.getElementById("Month-and-Year");
+const humidityElement = document.querySelector(".humidity");
+const input = document.getElementById("location");
+const cityName = document.querySelector(".city");
+const weatherIcon = document.getElementById("weather-icon");
+const temperatureDegree = document.querySelector(".temp-degree");
+const temperatureDescription = document.querySelector(".temp-description");
+const latElement = document.querySelector(".lat");
+const longElement = document.querySelector(".long");
+const errorContainer = document.querySelector(".error-container")
 
-let currentDateString = currentDate.toString();
-let getDay = currentDateString.split(" ").slice(0,1).join(" ");
-let getMonthAndYear = currentDateString.split(" ").slice(1,4).join(" ");
-
-let input = document.getElementById("location")
-
-presentDay.innerText = getDay;
-MonthAndYear.innerText = getMonthAndYear
-
-
-document.addEventListener("DOMContentLoaded", () =>{
-    navigator.geolocation.getCurrentPosition(AccessedLocation)
-})
-
+const apiKey = "d2ac27d2600f53d53f942008d727564f";
 let lat;
 let long;
 
+function getCurrentDateDetails() {
+  const currentDate = new Date();
+  const currentDateString = currentDate.toDateString();
+  const getDay = currentDateString.split(" ").slice(0, 1).join(" ");
+  const getMonthAndYear = currentDateString.split(" ").slice(1, 4).join(" ");
 
-function AccessedLocation(position){
-    if("geolocation" in navigator){
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-        fetchWeatherData(lat,long);
-    }
- 
-    else{
-        return "Geolocation is not supported"
-    }
-
+  presentDay.innerText = getDay;
+  monthAndYear.innerText = getMonthAndYear;
 }
 
-function fetchWeatherData(){
-
-let myApiKey = "d2ac27d2600f53d53f942008d727564f"
-const WeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myApiKey}`;
-
-fetch(WeatherUrl)
-.then(response => response.json())
-.then(data =>  console.log(data))
-
+function getLocationAndFetchWeatherData() {
+  navigator.geolocation.getCurrentPosition(AccessedLocation);
 }
 
-input.addEventListener("keypress",getWeatherDetails)
+function AccessedLocation(position) {
+  if ("geolocation" in navigator) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    fetchWeatherData(lat, long);
+  } else {
+    console.error("Geolocation is not supported");
+  }
+}
 
- function getWeatherDetails(e){
- if(e.key === "Enter"){
+function fetchWeatherData(lat, long) {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+  getWeatherDetailsFromApi(weatherUrl);
+}
+
+async function getWeatherDetailsFromApi(weatherUrl) {
+  try {
+    const response = await fetch(weatherUrl);
+    if (!response.ok) {
+      throw new Error("Weather data not available for this location");
+    }
+    const data = await response.json();
+    console.log(data)
+    displayData(data);
+  } catch (error) {
+    console.error(error);
+    displayError(Error);
+  }
+}
+
+function displayData(data) {
+  cityName.innerText = data.name;
+  temperatureDescription.innerText = data.weather[0].description;
+  const tempDeg = parseFloat(data.main.temp - 273).toFixed(1);
+  temperatureDegree.innerText = `${tempDeg}°C`;
+
+  const parsedLat = parseFloat(data.coord.lat).toFixed(1);
+  const parsedLon = parseFloat(data.coord.lon).toFixed(1);
+  latElement.innerText = `${parsedLat}°C`;
+  longElement.innerText = `${parsedLon}°C`;
+
+  const humidity = data.main.humidity;
+  humidityElement.innerText = `${humidity}%`;
+
+  const icon = data.weather[0].icon;
+  weatherIcon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${icon}@2x.png`
+  );
+}
+
+function displayError(Err) {
+  
+  if(Err = "Weather data not available for this location"){
+    errorContainer.style.display = "block"
+
+    setTimeout(()=>{
+      if(errorContainer.style.display = "block"){
+        errorContainer.style.display = "none"
+        let inputValue = input.value;
+      }
     
- }
+    },3000)
+
+ 
+  }
+  
  }
 
+
+// Initial setup
+getCurrentDateDetails();
+getLocationAndFetchWeatherData();
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const inputValue = input.value;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`;
+    getWeatherDetailsFromApi(weatherUrl);
+    dailyForecast()
+  }
+});
