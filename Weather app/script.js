194880f7085e535e8330cb454b842bd7b55a92d3
Bello Joseph <joexef87@gmail.com>
2023-08-15 23:58:15 +0100

    Update script.js

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,18 +1,11 @@
 const presentDay = document.getElementById("current-day");
 const monthAndYear = document.getElementById("Month-and-Year");
-const humidityElement = document.querySelector(".humidity");
 const input = document.getElementById("location");
-const cityName = document.querySelector(".city");
-const weatherIcon = document.getElementById("weather-icon");
 const temperatureDegree = document.querySelector(".temp-degree");
 const temperatureDescription = document.querySelector(".temp-description");
-const latElement = document.querySelector(".lat");
-const longElement = document.querySelector(".long");
-
-const day1 = document.querySelector("day-1");
-const day2 = document.querySelector("day-2");
-const day3 = document.querySelector("day-3");
-const day4 = document.querySelector("day-4");
+const WindSpeed = document.querySelector(".Wind-speed");
+const Pressure = document.querySelector(".prec");
+const humidityElement = document.querySelector(".humidity");
 
 const daysOfTheWeek = [
   "Sunday",
@@ -67,27 +60,31 @@ async function getWeatherDetailsFromApi(weatherUrl) {
       throw new Error("Weather data not available for this location");
     }
     const data = await response.json();
-    console.log(data);
     displayData(data);
+    console.log(data);
   } catch (error) {
-    displayError(Error);
+    displayError(error);
+    console.log(error);
   }
 }
 
 function displayData(data) {
+
+  const cityName = document.querySelector(".city");
   cityName.innerText = data.name;
+
   temperatureDescription.innerText = data.weather[0].description;
   const tempDeg = parseFloat(data.main.temp - 273).toFixed(1);
   temperatureDegree.innerText = `${tempDeg}°C`;
 
   const parsedLat = parseFloat(data.coord.lat).toFixed(1);
   const parsedLon = parseFloat(data.coord.lon).toFixed(1);
-  latElement.innerText = `${parsedLat}°C`;
-  longElement.innerText = `${parsedLon}°C`;
 
-  const humidity = data.main.humidity;
-  humidityElement.innerText = `${humidity}%`;
+  humidityElement.innerText = `${data.main.humidity}%`;
+  WindSpeed.innerText = `${data.wind.speed}m/s`;
+  Pressure.innerText = `${data.main.pressure}pa`
 
+  const weatherIcon = document.getElementById("weather-icon");
   const icon = data.weather[0].icon;
   weatherIcon.setAttribute(
     "src",
@@ -95,32 +92,42 @@ function displayData(data) {
   );
 }
 
-function displayError(Err) {
-  if ((Err = "Weather data not available for this location")) {
+function displayError(error) {
+  const errorContainer = document.querySelector(".error-container");
+  if (
+    error.message === "Weather data not available for this location" ||
+    error.message === "Cannot access data in this location"
+  ) {
     errorContainer.style.display = "block";
 
     setTimeout(() => {
-      if ((errorContainer.style.display = "block")) {
+      if (errorContainer.style.display === "block") {
         errorContainer.style.display = "none";
-        let inputValue = input.value;
       }
     }, 3000);
   }
 }
-
 // Initial setup
 getCurrentDateDetails();
 getLocationAndFetchWeatherData();
-input.addEventListener("keypress", (e) => {
+
+const handleKeyPress = async (e) => {
+  const DetailsContainer = document.querySelector(".fetching-details");
   if (e.key === "Enter") {
     const inputValue = input.value;
     const wweatherByCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`;
     const WeatherForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}`;
 
-    getWeatherDetailsFromApi(wweatherByCityUrl);
-    dailyForecast(WeatherForecast);
+    DetailsContainer.style.display = "block";
+
+    await getWeatherDetailsFromApi(wweatherByCityUrl);
+    await dailyForecast(WeatherForecast);
+
+    DetailsContainer.style.display = "none";
   }
-});
+};
+
+input.addEventListener("keypress", handleKeyPress);
 
 async function dailyForecast(WeatherForecast) {
   try {
@@ -129,7 +136,7 @@ async function dailyForecast(WeatherForecast) {
       throw new Error("Cannot access data in this location");
     }
     let data = await response.json();
-    console.log(data);
+
     displayForecast(data);
   } catch (error) {
     console.log(error);
@@ -137,6 +144,67 @@ async function dailyForecast(WeatherForecast) {
 }
 
 function displayForecast(data) {
-  let dailyForecastArray = data.list;
-  let DateTxt = data.list[0].dt_txt;
+  let dailyForecast;
+  let DateArray = [];
+  dailyForecast = data.list.filter((dates) => {
+    const uniqueDates = new Date(dates.dt_txt).getDate();
+    if (!DateArray.includes(uniqueDates)) {
+      return DateArray.push(uniqueDates);
+    }
+  });
+  inputForecastData(dailyForecast);
+}
+
+function inputForecastData(data) {
+  
+  let DailyTempArr = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const uniqueTemp = data[i].main.temp;
+    DailyTempArr.push(uniqueTemp);
+  }
+
+  const uniqueTemperatures = new Set(DailyTempArr);
+  const uniqueTemperaturesArray = Array.from(uniqueTemperatures);
+
+
+  for(let i = 0; i <= 3 ; i++){ 
+    document.querySelector(`.temp${i + 1}`).innerText = parseFloat(uniqueTemperaturesArray[i] - 273).toFixed(1);
+  }
+
+  console.log(uniqueTemperaturesArray)
+
+  let icon1 = data[0].weather[0].icon;
+  let icon2 = data[1].weather[0].icon;
+  let icon3 = data[2].weather[0].icon;
+  let icon4 = data[3].weather[0].icon;
+
+  document
+    .querySelector(".weather-forecast-icon")
+    .setAttribute("src", `https://openweathermap.org/img/wn/${icon1}@2x.png`);
+
+  document
+    .querySelector(".weather-forecast-icon-1")
+    .setAttribute("src", `https://openweathermap.org/img/wn/${icon2}@2x.png`);
+
+  document
+    .querySelector(".weather-forecast-icon-2")
+    .setAttribute("src", `https://openweathermap.org/img/wn/${icon3}@2x.png`);
+
+  document
+    .querySelector(".weather-forecast-icon-3")
+    .setAttribute("src", `https://openweathermap.org/img/wn/${icon4}@2x.png`);
+
+  let dayArray = [];
+  for (let i = 0; i < data.length; i++) {
+    const timestamp = data[i].dt * 1000;
+    const date = new Date(timestamp);
+    const weekDays = date.getDay();
+
+    dayArray.push(daysOfTheWeek[weekDays].split("").slice(0, 3).join(""));
+
+    for (let i = 1; i <= 3; i++) {
+      document.querySelector(`.day-${i + 1}`).innerText = dayArray[i];
+    }
+  }
 }
